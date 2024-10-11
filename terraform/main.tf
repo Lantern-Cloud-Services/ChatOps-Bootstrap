d360b8cc3dfcb9352542e501eeb7e4151f957d70
jdvcDev <59450481+jdvcDev@users.noreply.github.com>
2024-10-11 05:21:40 +0000

    terra

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,10 @@
 terraform {
   required_version = ">= 1.5.7"
   backend "azurerm" {
-    resource_group_name  = "demo-lcs2-dev-chatops-rg1"
+    resource_group_name = "demo-lcs2-dev-chatops-rg1"
     storage_account_name = "demochatopsbackendsa1"
-    container_name       = "demo-chatops-terraform-state"
-    key                  = "${var.project}-${var.environment}-state.tfstate"
-  }
-
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      # Root module should specify the maximum provider version
-      # The ~> operator is a convenient shorthand for allowing only patch releases within a specific minor release.
-      version = "~> 2.26"
-    }
+    container_name = "demo-chatops-terraform-state"
+    key = "${var.project}-${var.environment}-state.tfstate"
   }
 }
  
