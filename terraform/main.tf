fb40157979beb72897888b605245f098f273e042
jdvcDev <59450481+jdvcDev@users.noreply.github.com>
2024-10-11 17:04:43 +0000

    terra

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,8 @@ terraform {
     resource_group_name = "demo-lcs2-dev-chatops-rg1"
     storage_account_name = "demochatopsbackendsa1"
     container_name = "demo-chatops-terraform-state"
-    key = "${var.project}-${var.environment}-state.tfstate"
+    key = "chopsdemo-dev-state.tfstate"
+    #key = "${var.project}-${var.environment}-state.tfstate"
   }
 }
  
