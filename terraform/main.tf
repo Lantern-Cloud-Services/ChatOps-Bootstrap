e1088a67b055290abdd187c63c8d6076476954de
jdvcDev <59450481+jdvcDev@users.noreply.github.com>
2024-10-11 05:20:05 +0000

    terra

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,15 @@ terraform {
     container_name       = "demo-chatops-terraform-state"
     key                  = "${var.project}-${var.environment}-state.tfstate"
   }
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      # Root module should specify the maximum provider version
+      # The ~> operator is a convenient shorthand for allowing only patch releases within a specific minor release.
+      version = "~> 2.26"
+    }
+  }
 }
  
 provider "azurerm" {
