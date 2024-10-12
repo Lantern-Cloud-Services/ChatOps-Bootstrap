8a888b47ecaac9f6cdb4f80a9e34cfd18b83c7c3
jdvcDev <59450481+jdvcDev@users.noreply.github.com>
2024-10-12 03:29:13 +0000

    terra

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,7 +49,7 @@ resource "azurerm_linux_function_app" "example" {
   name                        = "example-function-app-${var.randomname}"
   location                    = azurerm_resource_group.example.location
   resource_group_name         = azurerm_resource_group.example.name
-  service_plan_id             = azurerm_app_service_plan.example.id
+  service_plan_id             = azurerm_service_plan.example.id
   storage_account_name        = azurerm_storage_account.example.name
   storage_account_access_key  = azurerm_storage_account.example.primary_access_key
   https_only                  = true
@@ -68,7 +68,7 @@ resource "random_pet" "prefix" {
   length = 1
 }
 
-resource "azurerm_service_plan" "defaultexample" {
+resource "azurerm_service_plan" "example" {
   name                = "chatopsfunsa${var.randomname}"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
