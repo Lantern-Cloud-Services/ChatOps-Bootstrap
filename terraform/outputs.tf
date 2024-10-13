da9befbf210650e5339490783ba29e04cff1ad0b
jdvcDev <59450481+jdvcDev@users.noreply.github.com>
2024-10-13 03:50:07 +0000

    terra

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,7 @@ output "resource_group_name" {
 output "storage_account_name" {
   value = azurerm_storage_account.example.name
 }
-
+/*
 output "function_app_name" {
   value = azurerm_linux_function_app.example.name
 }
@@ -14,7 +14,7 @@ output "function_app_default_hostname" {
   value = azurerm_linux_function_app.example.default_hostname
   description = "Deployed function app hostname"
 }
-
+*/
 #output "prefix" {
 #  value = random_pet.prefix
 #}
